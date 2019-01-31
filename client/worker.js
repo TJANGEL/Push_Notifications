768dcac4958ebadb34d5383dc43ad36293a95a73
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-31 09:01:26 -0500

    changed notification icon

--- client/worker.js
+++ client/worker.js
@@ -6,6 +6,7 @@ self.addEventListener("push", e => {
 
   self.registration.showNotification(data.title, {
     body: "Notified by TommyTom",
-    icon: ""
+    icon:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwis5a_jlpjgAhVG1VkKHZ53AKYQjRx6BAgBEAU&url=http%3A%2F%2Fwww.clker.com%2Fclipart-ta-circle.html&psig=AOvVaw1uonZrrYq6nlcBzxYYwTxc&ust=1549029515346595"
   });
 });
